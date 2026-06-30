17fedad7defab7f21f6cb8660e0717507f810334
2EK66 <ben668869@gmail.com>
2026-06-30 12:06:07 +0100

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:4000",
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+          supabase: ["@supabase/supabase-js"],
+        },
+      },
+    },
+  },
+});
